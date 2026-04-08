f9139f63c58ebbf52f15d2bb03f73fc3178f986a
Claas <claas@claas.dev>
2026-04-08 13:16:23 +0000

    Add generated code

---
+++ src/test/java/dev/claas/flat/FlatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.claas.flat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
