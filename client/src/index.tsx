6a2ef0097106a13a3b95692b60c05043b3f60f9c
Claas <claas@claas.dev>
2026-04-12 14:32:03 +0200

    Experiment with API

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,5 @@ import { render } from 'solid-js/web'
 import './index.css'
 import App from './App.tsx'
 
-const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(() => <App />, document.body)
